b0f99ee7dfd106e460f5ccd7107a7565c3cbadcf
Ashutosh Kale <ashutosh.kale@hotmail.com>
2014-10-05 21:43:47 -0700

    Merged anu, sayali, ashu, saumil work
    Screens added
    ConfigureGateway
    EditSwitch
    EditUser
    FavoriteScreen
    Home
    ManageGateway
    ManageSwitch

--- Mobile-ControlledLighting/src/com/advanchip/mobile_controlledlighting/HomeActivity.java
+++ Mobile-ControlledLighting/src/com/advanchip/mobile_controlledlighting/HomeActivity.java
@@ -1,21 +1,98 @@
 package com.advanchip.mobile_controlledlighting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class HomeActivity extends Activity {
 
+	private static Button manageUsersButton, manageGatewayButton, favoriteButton, manageSwitchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        manageUsersButton = (Button)findViewById(R.id.manageUsers);
+        manageGatewayButton = (Button)findViewById(R.id.manageGateways);
+        favoriteButton = (Button)findViewById(R.id.favorites);
+        manageSwitchButton = (Button)findViewById(R.id.manageSwitches);
+        
+        manageUsersButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent intent = new Intent(HomeActivity.this, EditUser.class);		
+				startActivity(intent);
+			}
+		});
+        
+        manageGatewayButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent intent = new Intent(HomeActivity.this, ManageGateway.class);		
+				startActivity(intent);
+			}
+		});
+        
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent intent = new Intent(HomeActivity.this, FavoriteActivity.class);	
+				startActivity(intent);
+			}
+		});
+        
+        manageSwitchButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+
+				Intent intent = new Intent(HomeActivity.this, EditSwitchActivity.class);	
+				startActivity(intent);
+			}
+		});
+        
+        
+       
+        
+        
+        
+        
+   
+        
+        
     }
+    
+    private OnClickListener onClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.button1:
+                     //DO something
+                break;
+                case R.id.button2:
+                     //DO something
+                break;
+                case R.id.button3:
+                     //DO something
+                break;
+            }
+
+      }
+   };
 
 
-    @Override
+    
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
